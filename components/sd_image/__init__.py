bdb6169d674db51d97e7eb3af79ecfc2961a81a4
youkorr <96953496+youkorr@users.noreply.github.com>
2025-08-09 01:26:09 +0200

    Update __init__.py

--- components/sd_image/__init__.py
+++ components/sd_image/__init__.py
@@ -25,21 +25,65 @@ IMAGE_TYPES = {
     "RGB": ImageType.IMAGE_TYPE_RGB,
 }
 
-CONFIG_SCHEMA = cv.Schema({
+CONF_TRANSPARENCY = "transparency"
+CONF_BYTE_ORDER = "byte_order"
+CONF_INVERT_ALPHA = "invert_alpha"
+CONF_DITHER = "dither"
+
+TRANSPARENCY_TYPES = {
+    "OPAQUE": 0,
+    "CHROMA_KEY": 1,
+    "ALPHA_CHANNEL": 2,
+}
+
+BYTE_ORDER_TYPES = {
+    "BIG_ENDIAN": True,
+    "LITTLE_ENDIAN": False,
+}
+
+DITHER_TYPES = {
+    "NONE": 0,
+    "FLOYDSTEINBERG": 1,
+}
+
+# Schéma pour une seule image
+SINGLE_IMAGE_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SDImage),
     cv.Required(CONF_PATH): cv.string,
     cv.Optional(CONF_WIDTH): cv.positive_int,
     cv.Optional(CONF_HEIGHT): cv.positive_int,
     cv.Optional(CONF_RESIZE): cv.dimensions,
     cv.Optional(CONF_TYPE, default="RGB565"): cv.enum(IMAGE_TYPES, upper=True),
+    cv.Optional(CONF_TRANSPARENCY, default="OPAQUE"): cv.enum(TRANSPARENCY_TYPES, upper=True),
+    cv.Optional(CONF_BYTE_ORDER, default="LITTLE_ENDIAN"): cv.enum(BYTE_ORDER_TYPES, upper=True),
+    cv.Optional(CONF_INVERT_ALPHA, default=False): cv.boolean,
+    cv.Optional(CONF_DITHER, default="NONE"): cv.enum(DITHER_TYPES, upper=True),
 }).extend(cv.COMPONENT_SCHEMA)
 
+# Schéma principal - peut être une liste d'images
+CONFIG_SCHEMA = cv.Any(
+    cv.ensure_list(SINGLE_IMAGE_SCHEMA),  # Liste d'images
+    SINGLE_IMAGE_SCHEMA,  # Image unique
+)
+
 async def to_code(config):
+    # Si c'est une liste d'images
+    if isinstance(config, list):
+        for image_config in config:
+            await process_single_image(image_config)
+    else:
+        await process_single_image(config)
+
+async def process_single_image(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
     cg.add(var.set_path(config[CONF_PATH]))
     cg.add(var.set_image_type(config[CONF_TYPE]))
+    cg.add(var.set_transparency(config[CONF_TRANSPARENCY]))
+    cg.add(var.set_dither(config[CONF_DITHER]))
+    cg.add(var.set_invert_alpha(config[CONF_INVERT_ALPHA]))
+    cg.add(var.set_big_endian(config[CONF_BYTE_ORDER]))
     
     if CONF_RESIZE in config:
         cg.add(var.set_resize(config[CONF_RESIZE][0], config[CONF_RESIZE][1]))
